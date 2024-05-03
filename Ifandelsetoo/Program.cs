b273fd0cd0e1ef7a4e4573a41f2f04b121f3520a
simo <simojuku@gmail.com>
2024-05-03 12:54:56 +0300

    hhe

---
+++ Ifandelsetoo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ifandelsetoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("üks või kaks?");
+            int  i = 3;
+            int o= 5;
+            if (o > i)
+            {
+                Console.WriteLine("o on suurem kui i. aga kas  o on võrdne 5ga?");
+                if(o  == 5) 
+                {
+                    Console.WriteLine("yep");
+                }
+            }
+            else
+            {
+                Console.WriteLine("nah");
+            }
+
+        }
+    }
+}
