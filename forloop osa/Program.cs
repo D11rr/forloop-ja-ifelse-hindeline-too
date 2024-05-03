e8d1522fe384421b80516aba686e75000ced83dc
simo <simojuku@gmail.com>
2024-05-03 12:30:00 +0300

    llll

---
+++ forloop osa/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace forloop_osa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vaata seda");
+            int i = 64;
+            for (i = 1; i <= 69; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
